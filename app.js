ae444137c17769fa300fb79fa4eb30a08c9b6895
Iva678 <123812601+Iva678@users.noreply.github.com>
2023-01-31 15:05:19 +0700

    Add new files

---
+++ app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const targetDiv = document.getElementById("toggle");
+const btn = document.getElementById("tombol");
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#f191ac';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
